0bf709756b7cecd86c8aea183763853d32889c50
yczn@testing <yczn@testing>
2021-10-04 09:51:53 +0800

    add ctl client

--- cmd/supd/script/install_bin.sh
+++ cmd/supd/script/install_bin.sh
@@ -4,15 +4,18 @@ case "$1" in
     "install")
         mkdir -p /usr/bin
         cp -rf ../supd /usr/bin
+        cp -rf ../supc /usr/bin
         mkdir -p /etc/supd/conf.d
         cp -rf ../etc/supd/supd.ini /etc/supd
     ;;
     "upgrade")
         cp -rf ../supd /usr/bin
+        cp -rf ../supc /usr/bin
     ;;
     "clean")
         if [ -f "/usr/bin/supd" ];then
-            sudo rm -rf /usr/bin/supd
+            rm /usr/bin/supd
+            rm /usr/bin/supc
         fi
     ;;
 esac
